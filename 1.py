cecd8926185f36bd7da6ee72fbcaf9890b74a3d7
eummbee <eumbee1016@gmail.com>
2024-11-29 13:51:01 +0900

    python1

---
+++ 1.py
@@ -0,0 +1,21 @@
+# 아래도 주석이고,
+'''
+print("Hello World!")
+'''
+# 이것도 주석입니다.
+
+print("Hello", "python", 1, 2, "abcedf", sep="\n") #sep=" " 이걸로 출력 방식을 바꿀 수 있다.
+print() #줄바꿈
+print("hello")
+print("안녕하세요", end="\n")
+print("코딩온입니다.")
+
+ive = "I AM"
+print(ive)
+ive = "장원영"
+print(ive)
+
+#식별자 규칙 -> 일관되게 정한다.
+#my_home : snake_case
+#MyHome : CamelCase
+#myHome : PascalCase
