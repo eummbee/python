fc2971dbb8e0a09f7de80acb17aba61fa6ca36bd
eummbee <eumbee1016@gmail.com>
2024-12-03 09:33:08 +0900

    파이썬 기초

---
+++ 01_basic/1.py
@@ -0,0 +1,22 @@
+# python 기초
+# 아래도 주석이고,
+'''
+print("Hello World!")
+'''
+# 이것도 주석입니다.
+
+print("Hello", "python", 1, 2, "abcedf", sep=" ") #sep=" " 이걸로 출력 방식을 바꿀 수 있다.
+print() #줄바꿈
+print("hello")
+print("안녕하세요", end="\n") #end="" 끝부분 규칙
+print("코딩온입니다.")
+
+ive = "I AM"
+print(ive)
+ive = "장원영"
+print(ive)
+
+#식별자 규칙 -> 일관되게 정한다.
+#my_home : snake_case
+#MyHome : CamelCase
+#myHome : PascalCase
